fab7f14e15026b1524347464d3f85826cef03a6e
wirthual <wirthra@gmail.com>
2021-11-02 05:37:16 +0100

    move to deutschland folder

--- rename_generated_code.py
+++ rename_generated_code.py
@@ -1,9 +1,14 @@
+import distutils
 import glob
+import os
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    api = {'name': 'autobahn'}
-    generation_base_path = f"./python-client/"
+    api = {"name": "autobahn"}
+    generation_base_path = f"./python-client"
+    generation_module_path = os.path.join(generation_base_path, api["name"])
+
+    destination_path = os.path.join(".python-client/deutschland/", api["name"])
 
     # Go through generated package and make imports relative to deutschland package in all files ending with .py and .md
     file_types_for_substitution = [".py", ".md"]
@@ -17,12 +22,15 @@ if __name__ == '__main__':
             print(filepath)
             with open(filepath) as file:
                 s = file.read()
-            s = s.replace(
-                f'from {api["name"]}', f'from deutschland.{api["name"]}'
-            )
+            s = s.replace(f'from {api["name"]}', f'from deutschland.{api["name"]}')
             s = s.replace(
                 f'import {api["name"]}',
                 f'from deutschland import {api["name"]}',
             )
             with open(filepath, "w") as file:
                 file.write(s)
+
+    os.makedirs(os.path.join(generation_base_path,"deutschland"),exist_ok=True)
+
+    # Copy the module to the deutschland folder
+    distutils.dir_util.copy_tree(generation_module_path, destination_path)
